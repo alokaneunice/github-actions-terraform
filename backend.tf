94bc9d880013ad5082ff1c8d0f711d96bf1e1ed7
alokaneunice <alokaneunice@outlook.com>
2025-07-21 14:24:04 +0100

    my commit

---
+++ backend.tf
@@ -0,0 +1,15 @@
+terraform {
+  backend "s3" {
+    bucket         = "my-tf-example-bucket-888"
+    region         = "us-east-2"
+    key            = "s3-github-actions/terraform.tfstate"
+    encrypt = true
+  }
+  required_version = ">=0.13.0"
+  required_providers {
+    aws = {
+      version = ">= 2.7.0"
+      source = "hashicorp/aws"
+    }
+  }
+}
